acfc9086164cd725b6c1248f11b0ca1278a02633
pavanleton <pavankumarkpkp@gmail.com>
2023-04-15 21:42:41 +0530

    update: add script to get NFT Transfers

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,57 @@
 import dotenv from 'dotenv'
+import { ethers } from 'ethers'
 
 // CONFIGS
 import { Provider } from '@/config/provider'
+import { setup } from '@/setup'
 
 dotenv.config()
 
-Provider?.detectNetwork().then((network) => {
-  console.log(`The chain ID is ${network.chainId}`)
-})
+const abi = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+]
+const contract = new ethers.Contract(setup?.contracts[0].address, abi, Provider)
 
-Provider.getBlockNumber().then((blockNumber) => {
-  console.log(`The Current Block is ${blockNumber}`)
+async function getNftTransferLogs(fromBlock = 26474083, toBlock = 'latest') {
+  const filter = {
+    address: setup?.contracts[0].address,
+    fromBlock,
+    toBlock,
+    topics: [ethers.utils.id('Transfer(address,address,uint256)'), null, null],
+  }
+  const logs = await Provider.getLogs(filter)
+  return logs.slice(0, 1).map((log) => {
+    const event = contract.interface.parseLog(log)
+    return {
+      from: event.args[0],
+      to: event.args[1],
+      tokenId: event.args[2].toString(),
+    }
+  })
+}
+
+// Example usage
+getNftTransferLogs(setup?.contracts[0].startBlock).then((logs) => {
+  console.log(logs)
 })
---
+++ src/setup.ts
@@ -0,0 +1,9 @@
+export const setup = {
+  contracts: [
+    {
+      name: 'Chumbi',
+      address: '0x86607B5a7aFF30a02bC6442455a3213085B13E76',
+      startBlock: 26474083,
+    },
+  ],
+}
