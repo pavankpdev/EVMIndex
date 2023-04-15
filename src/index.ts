2e6cd96cce4608502b9444c043391fcf6eb55e3f
pavanleton <pavankumarkpkp@gmail.com>
2023-04-15 14:40:24 +0530

    feat: Setup Ether provider

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const message = 'Hello, world! '
-console.log(message)
+import dotenv from 'dotenv'
+
+// CONFIGS
+import { Provider } from '@/config/provider'
+
+dotenv.config()
+
+Provider?.detectNetwork().then((network) => {
+  console.log(`The chain ID is ${network.chainId}`)
+})
+
+Provider.getBlockNumber().then((blockNumber) => {
+  console.log(`The Current Block is ${blockNumber}`)
+})
