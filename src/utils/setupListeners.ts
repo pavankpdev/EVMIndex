9e181279fffed6b0e4d7d9cea5d69c3af9a743ad
pavanleton <pavankumarkpkp@gmail.com>
2023-04-20 10:37:25 +0530

    update: add test webhook

--- src/utils/setupListeners.ts
+++ src/utils/setupListeners.ts
@@ -2,6 +2,7 @@ import { ethers } from 'ethers'
 import { Provider } from '@/config/provider'
 import { Contract } from '@/types'
 import Transfers from '@/db/models/Transfers'
+import axios from 'axios'
 
 export async function setupListeners(contracts: Contract[]) {
   const provider = Provider
@@ -33,7 +34,17 @@ export async function setupListeners(contracts: Contract[]) {
         console.log(
           `Event ${eventName} for contract at ${address}: from ${from}, to ${to}, tokenId ${tokenId}`
         )
-        // TODO: Call webhooks here
+        await axios({
+          method: 'post',
+          url: 'https://webhook.site/c9e34d6f-ebdb-486b-81e1-8ddb93fad649', // test webhook
+          data: {
+            from,
+            to,
+            tokenId: tokenId.toString(),
+            eventName,
+            contract: address,
+          },
+        })
       })
     })
   })
