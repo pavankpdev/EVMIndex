8ef47c182a623db39247ba95a6f771d17e9ec9a1
pavanleton <pavankumarkpkp@gmail.com>
2023-04-28 23:56:14 +0530

    add TODOs and FIXME

--- packages/indexer/src/scripts/build.ts
+++ packages/indexer/src/scripts/build.ts
@@ -1,8 +1,5 @@
-import gqlToMongoDB from 'graphql-to-mongodb';
 import {loadSchemaSync} from "@graphql-tools/load";
 import {GraphQLFileLoader} from "@graphql-tools/graphql-file-loader";
-import {MongoClient} from "mongodb";
-import {addResolversToSchema} from "@graphql-tools/schema";
 import {join} from "path";
 import {importSchema} from "graphql-import"
 
