61b5ffcc71e327fc2857a3ec26b3fa35bcd7b6d2
pavanleton <pavankumarkpkp@gmail.com>
2023-04-29 21:50:20 +0530

    WIP: interate over executable schema to generate Types

--- packages/indexer/src/scripts/build.ts
+++ packages/indexer/src/scripts/build.ts
@@ -12,32 +12,32 @@ const schema = makeExecutableSchema({ typeDefs });
 console.log('=============================')
 console.log(schema.getType('Approval')?._fields?.id)
 
-// const models = composeWithMongoose(
-//     mongoose.model('Approval', new mongoose.Schema({
-//
-//     })),
-//     schemaComposer as any
-// );
-//
-//
-// mongoose.connect('mongodb://localhost:27017/nft').then(() => {
-//     models.getResolver('createOne').resolve({
-//         args: {
-//             record: {
-//                 id: 'Approval',
-//                 owner: '0x123',
-//                 approved: '0x123',
-//                 tokenId: '123',
-//                 blockNumber: '12',
-//                 blockTimestamp: '12',
-//                 transactionHash: '12',
-//             }
-//         }
-//     })
-//         .then((res: any) => {
-//             console.log(res)
-//         })
-// })
-//
-//
-//
+const models = composeWithMongoose(
+    mongoose.model('Approval', new mongoose.Schema({
+
+    })),
+    schemaComposer as any
+);
+
+
+mongoose.connect('mongodb://localhost:27017/nft').then(() => {
+    models.getResolver('createOne').resolve({
+        args: {
+            record: {
+                id: 'Approval',
+                owner: '0x123',
+                approved: '0x123',
+                tokenId: '123',
+                blockNumber: '12',
+                blockTimestamp: '12',
+                transactionHash: '12',
+            }
+        }
+    })
+        .then((res: any) => {
+            console.log(res)
+        })
+})
+
+
+
