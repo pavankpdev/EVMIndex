bb2776242b8c2b1b7058eab6a5316182bf87e737
pavanleton <pavankumarkpkp@gmail.com>
2023-04-29 22:32:59 +0530

    update: iterate and create JSON to generate TYPES

--- packages/indexer/src/scripts/build.ts
+++ packages/indexer/src/scripts/build.ts
@@ -9,35 +9,47 @@ const { makeExecutableSchema } = require('@graphql-tools/schema');
 const schemaPath = join(__dirname, '../schema.graphql');
 const typeDefs = importSchema(schemaPath);
 const schema = makeExecutableSchema({ typeDefs });
-console.log('=============================')
-console.log(schema.getType('Approval')?._fields?.id)
 
-const models = composeWithMongoose(
-    mongoose.model('Approval', new mongoose.Schema({
-
-    })),
-    schemaComposer as any
-);
-
-
-mongoose.connect('mongodb://localhost:27017/nft').then(() => {
-    models.getResolver('createOne').resolve({
-        args: {
-            record: {
-                id: 'Approval',
-                owner: '0x123',
-                approved: '0x123',
-                tokenId: '123',
-                blockNumber: '12',
-                blockTimestamp: '12',
-                transactionHash: '12',
-            }
-        }
-    })
-        .then((res: any) => {
-            console.log(res)
-        })
+const typesArray = schema.getType('Approval')?.astNode?.fields.map((field: any) => {
+    return {
+        name: field?.name?.value,
+        type: field?.type?.type?.name?.value || field?.type?.name?.value,
+        isRequired: field.type?.kind === 'NonNullType'
+    }
 })
 
+console.log(typesArray)
 
+// console.log(schema.getType('Approval')?._fields?.id?.astNode?.name?.value)
+// console.log(schema.getType('Approval')?._fields?.id?.astNode?.type?.kind)
+// console.log(schema.getType('Approval')?._fields?.id?.type?.ofType?.name)
 
+// const models = composeWithMongoose(
+//     mongoose.model('Approval', new mongoose.Schema({
+//
+//     })),
+//     schemaComposer as any
+// );
+//
+//
+// mongoose.connect('mongodb://localhost:27017/nft').then(() => {
+//     models.getResolver('createOne').resolve({
+//         args: {
+//             record: {
+//                 id: 'Approval',
+//                 owner: '0x123',
+//                 approved: '0x123',
+//                 tokenId: '123',
+//                 blockNumber: '12',
+//                 blockTimestamp: '12',
+//                 transactionHash: '12',
+//             }
+//         }
+//     })
+//         .then((res: any) => {
+//             console.log(res)
+//         })
+// })
+//
+//
+//
