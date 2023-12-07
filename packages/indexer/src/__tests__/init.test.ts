2022d1f670d3007b068f34c2c90ddc4baa81ddbf
pavanleton <pavankumarkpkp@gmail.com>
2023-12-07 22:22:31 +0530

    test: add init test case

--- packages/indexer/src/__tests__/init.test.ts
+++ packages/indexer/src/__tests__/init.test.ts
@@ -1,5 +1,13 @@
-describe('My Test Suite', () => {
-    it('My Test Case', () => {
-        expect(true).toBe(true);
+import { EVMIndex } from '../';
+
+describe('EVMIndex Class', () => {
+    let evmIndex: EVMIndex;
+
+    beforeEach(() => {
+        evmIndex = new EVMIndex();
+    });
+
+    it('should be able to create an instance', () => {
+        expect(evmIndex).toBeInstanceOf(EVMIndex);
     });
 });
--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -1,3 +1,4 @@
-export class EVMIndex {
 
+export class EVMIndex {
+    constructor() {}
 }
