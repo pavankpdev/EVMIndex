fa66df8ce30ca90ea102ef789c33addae3610085
pavanleton <pavankumarkpkp@gmail.com>
2023-12-11 10:57:28 +0530

    feat: add a method to register Live sync for a contract from EVMIndex

--- packages/indexer/src/index.ts
+++ packages/indexer/src/index.ts
@@ -1,4 +1,26 @@
 import "fake-indexeddb/auto";
-export class EVMIndex {
-    constructor() {}
+import {LiveIndexer} from "@/lib/indexer/live";
+import {Log} from "viem";
+export class EVMIndex extends LiveIndexer {
+    constructor(
+        rpc: string
+    ) {
+        super(rpc);
+    }
+
+    public async addLiveSync(
+        address: `0x${string}`,
+        event: string,
+        callback: (logs: Log[]) => void
+    ): Promise<void> {
+        try {
+            console.log(`[Live Sync] Indexing ${address} for ${event} events`);
+            await this.watchEvent(address, event, callback)
+            return;
+        } catch (e: unknown) {
+            console.log(`[Live Sync] Failed to index ${address} for ${event} events`);
+            console.log(e);
+        }
+    }
+
 }
