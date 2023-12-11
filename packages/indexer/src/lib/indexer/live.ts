e86d6b510e29d61f9e2b1f4bd795ff20ad0b815b
pavanleton <pavankumarkpkp@gmail.com>
2023-12-11 10:56:57 +0530

    feat: add LiveIndexer to watch for live events

---
+++ packages/indexer/src/lib/indexer/live.ts
@@ -0,0 +1,25 @@
+import {Provider} from "../provider";
+import {Log, parseAbiItem, WatchEventReturnType} from "viem";
+
+export class LiveIndexer extends Provider {
+
+    private eventWatchers: Record<string, WatchEventReturnType> = {};
+
+    constructor(rpc: string) {
+        super(rpc);
+    }
+
+    protected async watchEvent(
+        address: `0x${string}`,
+        event: string,
+        logsHandler: (logs: Log[]) => void
+    ) {
+        this.eventWatchers[`${address}-${event}`] = this.getClient().watchEvent({
+            address,
+            onLogs: logsHandler,
+            // @ts-ignore
+            event: parseAbiItem(event),
+        });
+    }
+
+}
